771c663cb8e4adf5d433dea5a0d9f64ef4cd8c73
SashainSPb <74516322+SashainSPb@users.noreply.github.com>
2021-08-30 23:41:49 +0900

    job_description

---
+++ level_1/job_recommendation/job_recommendation.js
@@ -0,0 +1,14 @@
+function solution(table, languages, preference) {
+    var answer = '';
+    return answer;
+}
+
+
+/* 인덱스와 이중 배열을 이용해보자.
+1. table은 이중배열로 만들자
+
+["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"]
+
+
+
+*/
