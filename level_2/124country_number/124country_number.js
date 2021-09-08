569b7c5f8b3b25c3c16b638c021b99234d078a1b
SASHAinSPb <88parksw@gmail.com>
2021-09-08 23:15:06 +0900

    124country_number

---
+++ level_2/124country_number/124country_number.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = '';
+    
+    let switchedNumber = n.toString(3);
+    console.log(switchedNumber)
+    
+    // return answer;
+}
+
+/*
+1. 10진법에서 3진법으로 변환하는 것을 초점으로 두자 toString(n) method 이용
+2. 0 => 1, 1 => 2, 2 => 3으로 각각 변환하자
+3. 0이 가장 높은 자리에 올 경우를 예외 처리 해야함
+*/
